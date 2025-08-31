dd93ba949669ae96450a4c0a7040004c4e21ba65
TB <thrishbg15@gmail.com>
2025-08-31 04:15:19 +0100

    Clean code, remove redundants

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
-const mainBody = document.querySelector("main");
+window.addEventListener('load', function() {
+    const mainBody = document.querySelector("main");
 
-let toggleBtn;
-let wholePage;
-let hamburgerMenu;
+    let toggleBtn;
+    let wholePage;
+    let hamburgerMenu;
 
-function declare(){
-    toggleBtn = document.querySelector(".toggle-button");
-    wholePage = document.querySelector(".whole-page");
-    hamburgerMenu = document.querySelector(".hamburger-menu");
-}
-
-let dark = false;
-declare();
-
-function toggleAnimation() {
-    document.body.classList.add("stop-scrolling");
-    dark = !dark;
-    let clone = wholePage.cloneNode(true);   
-    if (dark) {
-        clone.classList.remove("light");
-        clone.classList.add("dark");
-    } else {
-        clone.classList.remove("dark");
-        clone.classList.add("light");
+    function declare(){
+        toggleBtn = document.querySelector(".toggle-button");
+        wholePage = document.querySelector(".whole-page");
+        hamburgerMenu = document.querySelector(".hamburger-menu");
     }
-    clone.classList.add("copy");
-    mainBody.appendChild(clone);
 
-    clone.addEventListener("animationend", () => {
-        document.body.classList.remove("stop-scrolling");
-        wholePage.remove();
-        clone.classList.remove("copy");
-        declare();
-        toggleEvents();
-    });
-}
+    let dark = false;
+    declare();
 
-function toggleEvents(){
-    if (toggleBtn) {
-        toggleBtn.addEventListener('click', toggleAnimation);
+    function toggleAnimation() {
+        document.body.classList.add("stop-scrolling");
+        dark = !dark;
+        let clone = wholePage.cloneNode(true);   
+        if (dark) {
+            clone.classList.remove("light");
+            clone.classList.add("dark");
+        } else {
+            clone.classList.remove("dark");
+            clone.classList.add("light");
+        }
+        clone.classList.add("copy");
+        mainBody.appendChild(clone);
+
+        clone.addEventListener("animationend", () => {
+            document.body.classList.remove("stop-scrolling");
+            wholePage.remove();
+            clone.classList.remove("copy");
+            declare();
+            toggleEvents();
+        });
     }
-    hamburgerMenu.addEventListener("click", () => {
-        wholePage.classList.toggle("active");
-    });
-}
 
-toggleEvents();
+    function toggleEvents(){
+        if (toggleBtn) {
+            toggleBtn.addEventListener('click', toggleAnimation);
+        }
+        hamburgerMenu.addEventListener("click", () => {
+            wholePage.classList.toggle("active");
+        });
+    }
+
+    toggleEvents();
+
+});
